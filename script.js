d43c973a0c925c6dd5bf42bd5058710fe55a44c7
Kumaisha-cyber <Kumaishakumaisha86@gmail.com>
2026-07-22 15:13:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,144 @@
+function analyzePassword() {
+
+    const password =
+        document.getElementById("password").value;
+
+    let score = 0;
+
+    const lengthCheck =
+        document.getElementById("length");
+
+    const uppercaseCheck =
+        document.getElementById("uppercase");
+
+    const lowercaseCheck =
+        document.getElementById("lowercase");
+
+    const numberCheck =
+        document.getElementById("number");
+
+    const symbolCheck =
+        document.getElementById("symbol");
+
+
+    if (password.length >= 8) {
+
+        score++;
+
+        lengthCheck.textContent =
+            "✅ At least 8 characters";
+
+    } else {
+
+        lengthCheck.textContent =
+            "❌ At least 8 characters";
+    }
+
+
+    if (/[A-Z]/.test(password)) {
+
+        score++;
+
+        uppercaseCheck.textContent =
+            "✅ Contains uppercase letter";
+
+    } else {
+
+        uppercaseCheck.textContent =
+            "❌ Contains uppercase letter";
+    }
+
+
+    if (/[a-z]/.test(password)) {
+
+        score++;
+
+        lowercaseCheck.textContent =
+            "✅ Contains lowercase letter";
+
+    } else {
+
+        lowercaseCheck.textContent =
+            "❌ Contains lowercase letter";
+    }
+
+
+    if (/[0-9]/.test(password)) {
+
+        score++;
+
+        numberCheck.textContent =
+            "✅ Contains a number";
+
+    } else {
+
+        numberCheck.textContent =
+            "❌ Contains a number";
+    }
+
+
+    if (/[^A-Za-z0-9]/.test(password)) {
+
+        score++;
+
+        symbolCheck.textContent =
+            "✅ Contains a special character";
+
+    } else {
+
+        symbolCheck.textContent =
+            "❌ Contains a special character";
+    }
+
+
+    const strength =
+        document.getElementById("strength");
+
+    const progress =
+        document.getElementById("progress");
+
+
+    if (score <= 2) {
+
+        strength.textContent =
+            "Strength: Weak";
+
+        progress.style.width =
+            "30%";
+
+    } else if (score <= 4) {
+
+        strength.textContent =
+            "Strength: Medium";
+
+        progress.style.width =
+            "65%";
+
+    } else {
+
+        strength.textContent =
+            "Strength: Strong";
+
+        progress.style.width =
+            "100%";
+    }
+}
+
+
+function togglePassword() {
+
+    const passwordBox =
+        document.getElementById("password");
+
+
+    if (passwordBox.type === "password") {
+
+        passwordBox.type =
+            "text";
+
+    } else {
+
+        passwordBox.type =
+            "password";
+    }
+}
